156e85d6fd6a4693f380ee6b1097699238bdf21d
Ben Fox-Moore <ben.foxmoore@gmail.com>
2021-01-28 19:20:02 +0100

    Prevent unnecessary JSON files being created
    The helper functions in json_repository.py currently cause JSON files to
    be created, even if they are not needed. This change avoids this by
    first checking for existence of the files, and if not found, returning
    empty lists/dicts.

--- nhltv_lib/json_repository.py
+++ nhltv_lib/json_repository.py
@@ -7,15 +7,10 @@ JSON_FILE_ENDING = ".json"
 # LIST
 
 
-def ensure_json_list_exists(listname: str) -> None:
-    if not os.path.exists(listname):
-        with open(listname, "w") as f:
-            json.dump(list(), f)
-
-
 def read_json_list(listname: str) -> List[Union[str, int]]:
     listname += JSON_FILE_ENDING
-    ensure_json_list_exists(listname)
+    if not os.path.isfile(listname):
+        return []
     with open(listname, "r") as f:
         return json.load(f)
 
@@ -32,15 +27,10 @@ def add_to_json_list(listname: str, addition: Union[str, int]):
 # DICT
 
 
-def ensure_json_dict_exists(dictname: str) -> None:
-    if not os.path.exists(dictname):
-        with open(dictname, "w") as f:
-            json.dump(dict(), f)
-
-
 def read_json_dict(dictname: str) -> Dict[str, str]:
     dictname += JSON_FILE_ENDING
-    ensure_json_dict_exists(dictname)
+    if not os.path.isfile(dictname):
+        return {}
     with open(dictname, "r") as f:
         return json.load(f)
 
--- tests/unit/test_json_repository.py
+++ tests/unit/test_json_repository.py
@@ -2,9 +2,7 @@ import json
 import pytest
 from nhltv_lib.json_repository import (
     read_json_list,
-    ensure_json_list_exists,
     read_json_dict,
-    ensure_json_dict_exists,
     add_to_json_dict,
     add_to_json_list,
 )
@@ -28,35 +26,26 @@ def m_open_dict(mocker):
     return m_open
 
 
-@pytest.fixture(autouse=True)
-def mock_json_list_exists(mocker):
-    return mocker.patch("nhltv_lib.json_repository.ensure_json_list_exists")
+def _m_isfile(mocker, ret):
+    return mocker.patch("os.path.isfile", return_value=ret)
 
 
-@pytest.fixture(autouse=True)
-def mock_json_dict_exists(mocker):
-    return mocker.patch("nhltv_lib.json_repository.ensure_json_dict_exists")
+@pytest.fixture
+def m_isfile_false(mocker):
+    return _m_isfile(mocker, False)
 
 
-@pytest.fixture(autouse=True)
-def mock_os_path_exists(mocker):
-    return mocker.patch("os.path.exists", return_value=False)
+@pytest.fixture
+def m_isfile_true(mocker):
+    return _m_isfile(mocker, True)
 
 
-def test_ensure_json_list_exists(mocker, m_open):
-    ensure_json_list_exists("test")
-    handle = m_open()
-    handle.write.assert_called_once_with(json.dumps(list()))
-
-
-def test_read_json_list(mocker, m_open):
+def test_read_json_list(mocker, m_open, m_isfile_true):
     assert read_json_list("test") == [392]
 
 
-def test_ensure_json_dict_exists(mocker, m_open):
-    ensure_json_dict_exists("test")
-    handle = m_open()
-    handle.write.assert_called_once_with(json.dumps(dict()))
+def test_read_json_list_nofile(mocker, m_open, m_isfile_false):
+    assert read_json_list("test") == []
 
 
 def test_add_to_json_list(mocker, m_open):
@@ -68,11 +57,21 @@ def test_add_to_json_list(mocker, m_open):
     mjson.assert_called_once_with([392, "boooo", 2000], m_open())
 
 
-def test_read_json_dict(mocker, m_open_dict):
+def test_add_to_json_list_nofile(mocker, m_open, m_isfile_false):
+    mjson = mocker.patch("json.dump")
+    add_to_json_list("test", 2000)
+    mjson.assert_called_once_with([2000], m_open())
+
+
+def test_read_json_dict(mocker, m_open_dict, m_isfile_true):
     assert read_json_dict("test") == {"392": "booboo"}
 
 
-def test_add_to_json_dict(mocker, m_open_dict):
+def test_read_json_dict_empty(mocker, m_open_dict, m_isfile_false):
+    assert read_json_dict("test") == {}
+
+
+def test_add_to_json_dict(mocker, m_open_dict, m_isfile_true):
     call = mocker.call
     add_to_json_dict("test", {"2000": "3000"})
     calls = [
@@ -89,7 +88,7 @@ def test_add_to_json_dict(mocker, m_open_dict):
     m_open_dict().write.assert_has_calls(calls)
 
 
-def test_add_to_json_dict_empty(mocker):
+def test_add_to_json_dict_empty(mocker, m_isfile_false):
     call = mocker.call
     m_open = mocker.mock_open(read_data="{}")
     mock_json_open(mocker, m_open)
